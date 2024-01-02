5432e71a53cb2e22564c00a941f2cf41f618d18e
baraka011 <65946662+baraka011@users.noreply.github.com>
2024-01-02 21:20:25 +0100

    Delete 源代码 directory

--- 源代码/Delete_CP.m
+++
@@ -1,8 +0,0 @@
-function output=Delete_CP(input,cp_length)
-input_temp = reshape(input,160,[]);
-[m,n]=size(input_temp);
-output=zeros(m-cp_length,n);
-for j=1:n
-    output(1:(m-cp_length),j)=input_temp((cp_length+1):m,j);
-end
-end
--- 源代码/qpsk_demodulation.m
+++
@@ -1,4 +0,0 @@
-function [data_outI,data_outQ]=qpsk_demodulation(data_in)
-data_outI = (1 - sign(real(data_in))) / 2;
-data_outQ = (1 - sign(imag(data_in))) / 2;
-end
